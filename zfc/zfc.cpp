dfd71bd0e0fd61648f0f498a995e31b0b1b4462b
breezer_xie <boringxie@gmail.com>
2020-10-29 17:23:47 +0800

    C语言学习

---
+++ zfc/zfc.cpp
@@ -0,0 +1,9 @@
+﻿# include <stdio.h>
+int main(void)
+{
+    char str[10];  //str是string的缩写, 即字符串
+    printf("请输入字符串：");
+    scanf_s("%s", str);  //输入参数是已经定义好的字符数组名
+    printf("输出结果：%s\n", str);
+    return 0;
+}
